6436e0e50f2da84670decda29ac00388a81573cd
Heesoo <heesoo.p1023@gmail.com>
2022-08-04 15:39:47 -0700

    create database and connect with Mongodb

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const MongoClient = require('mongodb').MongoClient;
 
+MongoClient.connect('mongodb+srv://admin:<password>@cluster0.gvddnzw.mongodb.net/?retryWrites=true&w=majority', (err, client) => {
+    app.listen(port, () => {
+        console.log("Listening on port 8080")
+    })
+})
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
@@ -17,6 +23,3 @@ app.post('/add', (req, res) => {
     res.send('Send Complete')
 })
 
-app.listen(port, () => {
-    console.log("Listening on port 8080")
-})
